f84a18ded5b7233ec1a4e47eb380ad76bb4db2e5
Dnax1998 <bajera.mateuszek@gmail.com>
2026-04-29 09:41:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ STATS_FILE = 'balance_history.json'
 
 # --- KONFIGURACJA ---
 INITIAL_CAPITAL = 1000.0       
-TRADE_AMOUNT_USDC = 120.0      
+TRADE_AMOUNT_USDC = 250.0      
 RSI_BUY_THRESHOLD = 50         
 RSI_SELL_THRESHOLD = 58        
 
